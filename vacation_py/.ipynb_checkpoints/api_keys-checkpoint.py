03307c2da877babb3e86fe338b504c6b4f83fde4
maryemackin <140846872+maryemackin@users.noreply.github.com>
2024-04-30 12:30:20 -0700

    updates to analysis statements, added rvalues

---
+++ vacation_py/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+weather_api_key = "f27b6614587941da959b0e70f7a6d5b8"
+
+# Geoapify API Key
+geoapify_key = "c3e0ad701cb445418d2e1576948cec85"
+
+#old_geoapify_key = "741ba2a282784c83a453fd33fc0d56f3"
